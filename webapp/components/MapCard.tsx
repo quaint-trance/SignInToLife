1ef378da8a4918d86b0ee2304da3bf622ef94598
skhyr <piotrmal2002@gmail.com>
2020-10-20 22:36:08 +0200

    Hackheroes

--- webapp/components/MapCard.tsx
+++ webapp/components/MapCard.tsx
@@ -1,46 +1,73 @@
 import { useState } from 'react'
 import styles from '../styles/MapCard.module.css'
 import { HighlightOff } from '@material-ui/icons'
-import {animated, useTransition} from 'react-spring'
+import {animated, useSpring} from 'react-spring'
 import { MdKeyboardBackspace} from 'react-icons/md'
 import { useRouter } from 'next/router'
+
+const config = {
+  tension: 170,
+  clamp: true
+}
 
 export default function MapCard({event, close}) {
   
   const [fullView, setFullView] = useState(false)
   const router = useRouter();
 
-  if(fullView) return (
-    <div className={styles.cardFull}>
-      <header className={styles.header}>
+  const animatedCard = useSpring({
+    height: fullView ? "100vh" : "30vh",
+    width: fullView ? "calc( 100vw )" : "calc( 95vw )",
+    padding: fullView ? "0rem" : "1rem",
+    margin: fullView ? "0px 0px" : "15px 2.5vw",
+    borderRadius: fullView ? "0px" : "10px",
+    config
+  })
+
+  const animatedTitle = useSpring({
+    fontSize: fullView ? "2rem" : "1.4rem",
+    config
+  })
+
+  const animatedPhotos = useSpring({
+    height: fullView ? "30vh" : "0vh",
+    config
+  })
+
+  const animatedNavbar = useSpring({
+    height: fullView ? "3rem" : "0rem",
+    config
+  })
+
+  const animatedHideOnFull = useSpring({
+    opacity: fullView ? 0 : 1,
+    config: { mass: 150, tension: 280, friction: 60, clamp: true }
+  })
+
+return (
+    <animated.div className={styles.cardFull} style={animatedCard}>
+      <animated.header className={styles.header} style={animatedNavbar}>
         <MdKeyboardBackspace onClick={()=>setFullView(false)}></MdKeyboardBackspace>
           <img src="/images/logo.png" alt="logo"/>
-      </header>
-      <div className={styles.Fphotos}>
+      </animated.header>
+      <animated.div className={styles.Fphotos} style={animatedPhotos}>
         {event.photos?.map(photo=>(
           <img src="" alt="e"/>
         ))}
-      </div>
+      </animated.div>
       <main className={styles.Fmain}>
-        <div className={styles.Ftitle}>
+        <animated.div className={styles.Ftitle} style={animatedTitle}>
           {event.name}
-        </div>
+        </animated.div>
         <div className={styles.Fdescription}>
           {event.description}
         </div>
       </main>
-    </div>
-  )
-  else return (
-    <div className={styles.card}>
-      <div className={styles.close} onClick={close}> <HighlightOff /></div>
-      <div className={styles.title}>{event.name}</div>
-      <div className={styles.description}>{event.description}</div>
-      <div className={styles.options}>
+      <animated.div className={styles.options} style={{display: fullView ? "none" : "block"}}>
         <div className={styles.date} >{(new Date(event.date)).toLocaleDateString()}</div>
         <button className={styles.button} onClick={()=>setFullView(true)}>more</button>
-      </div>
-    </div>
+        <animated.div className={styles.close} onClick={close} style={animatedHideOnFull}> <HighlightOff /></animated.div>
+      </animated.div>
+    </animated.div>
   )
 }
-
